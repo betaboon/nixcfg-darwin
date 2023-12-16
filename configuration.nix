0bfb01a27568019e5090da60ba7df0f3ede6ec03
betaboon <betaboon@0x80.ninja>
2023-12-16 18:55:56 +0100

    refactor: move configuration into separate configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }: {
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    vim
+  ];
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
+}
